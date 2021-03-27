096bf7907530c1000b5546823f449703aefd2131
Diundina_Maria <mary.diundina@gmail.com>
2021-03-27 16:46:55 +0500

    Add tests.

---
+++ tests/test_decode.py
@@ -0,0 +1,79 @@
+from my_lzma.decoder.lzma_decoder import LZMADecoder
+from pathlib import Path
+
+
+def cmp_files(file1: Path, file2: Path) -> None:
+    with file1.open() as f:
+        data_1 = f.read()
+    with file2.open() as f:
+        data_2 = f.read()
+    assert data_1 == data_2
+
+
+def test_decode_a():
+    in_file = Path().cwd() / 'tests' / 'a.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(in_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(out_file, Path().cwd() / 'tests' / 'a.txt')
+
+
+def test_decode_a_eos():
+    in_file = Path().cwd() / 'tests' / 'a_eos.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(in_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(out_file, Path().cwd() / 'tests' / 'a.txt')
+
+
+def test_decode_a_eos_and_size():
+    in_file = Path().cwd() / 'tests' / 'a_eos_and_size.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(in_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(out_file, Path().cwd() / 'tests' / 'a.txt')
+
+
+def test_decode_a_lp1_lc2_pb1():
+    in_file = Path().cwd() / 'tests' / 'a_lp1_lc2_pb1.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(in_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(out_file, Path().cwd() / 'tests' / 'a.txt')
+
+
+def test_bad_corrupted():
+    in_file = Path().cwd() / 'tests' / 'bad_corrupted.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    try:
+        decoder = LZMADecoder(in_file, out_file)
+        decoder.decode()
+    except:
+        return
+    assert False
+
+
+def test_bad_eos_incorrect_size():
+    in_file = Path().cwd() / 'tests' / 'bad_eos_incorrect_size.lzma'
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    try:
+        decoder = LZMADecoder(in_file, out_file)
+        decoder.decode()
+    except:
+        return
+    assert False
+
+
+def test_bad_incorrect_size():
+    in_file = Path().cwd() / 'bad_incorrect_size.lzma'
+    out_file = Path().cwd() / 'temp.txt'
+    try:
+        decoder = LZMADecoder(in_file, out_file)
+        decoder.decode()
+    except:
+        return
+    assert False
---
+++ tests/test_encode.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+from my_lzma.encoder.lzma_encoder import LZMAEncoder
+from my_lzma.decoder.lzma_decoder import LZMADecoder
+
+
+def cmp_files(file1: Path, file2: Path) -> None:
+    with file1.open() as f:
+        data_1 = f.read()
+    with file2.open() as f:
+        data_2 = f.read()
+    assert data_1 == data_2
+
+
+def test_a():
+    in_file = Path().cwd() / 'tests' / 'a.txt'
+    mid_file = Path().cwd() / 'tests' / 'temp.lzma'
+    encoder = LZMAEncoder(in_file, mid_file, 3, 0, 2, 1024 * 1024)
+    encoder.encode()
+    encoder.range_encoder.out_stream.file.close()
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(mid_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(in_file, out_file)
+
+
+def test_long_string():
+    in_file = Path().cwd() / 'tests' / 'long_string.txt'
+    mid_file = Path().cwd() / 'tests' / 'temp.lzma'
+    encoder = LZMAEncoder(in_file, mid_file, 3, 0, 2, 300)
+    encoder.encode()
+    encoder.range_encoder.out_stream.file.close()
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(mid_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(in_file, out_file)
+
+
+def test_rand_string():
+    in_file = Path().cwd() / 'tests' / 'rand_string.txt'
+    mid_file = Path().cwd() / 'tests' / 'temp.lzma'
+    encoder = LZMAEncoder(in_file, mid_file, 3, 0, 2, 300)
+    encoder.encode()
+    encoder.range_encoder.out_stream.file.close()
+    out_file = Path().cwd() / 'tests' / 'temp.txt'
+    decoder = LZMADecoder(mid_file, out_file)
+    decoder.decode()
+    decoder.out_window.out_stream.file.close()
+    cmp_files(in_file, out_file)
+
+
+def test_incorrect_lc():
+    in_file = Path().cwd() / 'tests' / 'long_string.txt'
+    out_file = Path().cwd() / 'tests' / 'temp.lzma'
+    try:
+        encoder = LZMAEncoder(in_file, out_file, -1, 0, 2, 1024)
+        encoder.encode()
+    except:
+        return
+    assert False
+
+
+def test_incorrect_lp():
+    in_file = Path().cwd() / 'tests' / 'long_string.txt'
+    out_file = Path().cwd() / 'tests' / 'temp.lzma'
+    try:
+        encoder = LZMAEncoder(in_file, out_file, 3, -1, 2, 1024)
+        encoder.encode()
+    except:
+        return
+    assert False
+
+
+def test_incorrect_pb():
+    in_file = Path().cwd() / 'tests' / 'long_string.txt'
+    out_file = Path().cwd() / 'tests' / 'temp.lzma'
+    try:
+        encoder = LZMAEncoder(in_file, out_file, 3, 0, -1, 1024)
+        encoder.encode()
+    except:
+        return
+    assert False
+
+
+def test_incorrect_dict_size():
+    in_file = Path().cwd() / 'tests' / 'long_string.txt'
+    out_file = Path().cwd() / 'tests' / 'temp.lzma'
+    try:
+        encoder = LZMAEncoder(in_file, out_file, 3, 0, 2, -1)
+        encoder.encode()
+    except:
+        return
+    assert False
